1ca82af49fa62b0026c393a50fdf3a9feb201e60
nomadicj <j@armstro.ca>
2019-01-01 12:15:45 +0000

    Using correct env var for build num

--- letsencrypt/cert.sh
+++ letsencrypt/cert.sh
@@ -12,7 +12,7 @@ tar -C /etc/letsencrypt/ -cvf certbundle.tar ./
 aws s3 cp certbundle.tar s3://config.nomadicj/$DOMAIN/cert/
 
 aws iam upload-server-certificate --path /cloudfront/ \
---server-certificate-name $DOMAIN-$BUILD \
+--server-certificate-name $DOMAIN-$CIRCLE_BUILD_NUM \
 --certificate-body file:///etc/letsencrypt/live/$DOMAIN/cert.pem \
 --certificate-chain file:///etc/letsencrypt/live/$DOMAIN/chain.pem \
 --private-key file:///etc/letsencrypt/live/$DOMAIN/privkey.pem
