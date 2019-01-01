2c5530cd91df1a627e4f5e31607ccd0aae2fad79
nomadicj <j@armstro.ca>
2019-01-01 11:39:03 +0000

    Pulling in Cert renew capability

---
+++ letsencrypt/cert.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+aws s3 sync s3://config.nomadicj/$DOMAIN/cert/ ./
+
+tar -C /etc/letsencrypt -xvf certbundle.tar
+
+certbot renew
+# certbot certonly -n --agree-tos --email admin@armstro.co --dns-route53 -d armstro.co
+
+tar -C /etc/letsencrypt/ -cvf certbundle.tar ./
+
+aws s3 cp certbundle.tar s3://config.nomadicj/$DOMAIN/cert/
+
+aws iam upload-server-certificate --path /cloudfront/ \
+--server-certificate-name $DOMAIN-$BUILD \
+--certificate-body file:///etc/letsencrypt/live/$DOMAIN/cert.pem \
+--certificate-chain file:///etc/letsencrypt/live/$DOMAIN/chain.pem \
+--private-key file:///etc/letsencrypt/live/$DOMAIN/privkey.pem
